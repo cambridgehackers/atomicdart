6785382b80f38789001def06c30f2598d41c5b28
Jamey Hicks <jamey.hicks@gmail.com>
2016-09-06 10:46:50 -0400

    gcd example

---
+++ gcd.dart
@@ -0,0 +1,33 @@
+import 'atomic.dart';
+
+class Gcd extends Module {
+  Reg<int> n;
+  Reg<int> m;
+  Gcd(int _n, int _m) : super("gcd") {
+    n = new Reg<int>(_n);
+    m = new Reg<int>(_m);
+
+    addRule("swap", () => (n.val > m.val && m.val != 0),
+      () {
+      n.val = m.val;
+      m.val = n.val;
+    });
+
+    addRule("sub", () => (n.val <= m.val && m.val != 0),
+        () => m.val = m.val - n.val);
+
+    addRule("result", () => m.val == 0,
+      () {
+      var gcd = n.val;
+      print("Gcd Result: $gcd");
+      finish();
+    });
+  }
+}
+
+void main() {
+  print("instantiating Gcd");
+  var gcd = new Gcd(3, 6);
+  gcd.run();
+  print("done");
+}
